4895be09a96822f09e821b8e28642cdbc7056c74
Szabson <szymonzaba18@gmail.com>
2026-03-06 09:39:24 +0100

    basic models to breakdowns

--- machine/models.py
+++ machine/models.py
@@ -12,5 +12,38 @@ class Machine(models.Model):
 
 
 class BreakDown(models.Model):
+    class Priority(models.TextChoices):
+        NONE = 'NONE', 'None'
+        LOW = 'LOW', 'Low'
+        MID = 'MID', 'Mid'
+        HIGH = 'HIGH', 'High'
+
     machine = models.ForeignKey(Machine, on_delete=models.SET_NULL, null=True, blank=True, related_name='breakdowns')
     date_added = models.DateTimeField(auto_now_add=True)
+    priority = models.CharField(max_length=4, choices=Priority, default=Priority.NONE)
+    reporter = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True, related_name='breakdowns')
+    description = models.CharField(max_length=1024, null=True, blank=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['-date_added']),
+        ]
+
+
+class BreakDownMove(models.Model):
+    class Status(models.TextChoices):
+        REPORTED = 'RP', 'Reported'
+        STARTED = 'ST', 'Started'
+        ENDED = 'ED', 'Ended'
+
+    break_down = models.ForeignKey(BreakDown, on_delete=models.CASCADE, related_name='history')
+    user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True)
+    status = models.CharField(max_length=2, choices=Status)
+    time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-time']
+        indexes = [
+            models.Index(fields=['time']),
+            models.Index(fields=['status'])
+        ]
